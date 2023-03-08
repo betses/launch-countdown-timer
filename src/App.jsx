39d956b78e69c4806fa4d545c1e6abaf3c9949e9
Betsegaw Sebsibe <betsesebe@gmail.com>
2023-03-08 16:03:01 +0300

    Add card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import "./App.css";
+import footer from './assets/pattern-hills.svg';
+import star from './assets/bg-stars.svg';
+import Timer from './component/Timer';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <div
+        className="h-screen flex flex-col items-center bg-[#1e1f29] py-32 space-y-28"
+        style={{ backgroundImage: `url(${star})` }}
+      >
+        <h1 className="text-white text-xl font-bold capitalize tracking-[0.449em] pt-1">
+          WE'RE LAUNCHING SOON
+        </h1>
+        <Timer />
+      </div>
+      <footer
+        className="absolute bg-cover bottom-0 left-0 w-full h-48"
+        style={{ backgroundImage: `url(${footer})` }}
+      ></footer>
+    </>
+  );
 }
 
 export default App;
